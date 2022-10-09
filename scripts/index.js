846c2541c1d91f8adef7db5b64a308d1e14d6379
see-why <iyadicy@gmail.com>
2022-10-09 23:20:35 +0100

    refactor: add templates for pop-up messages

--- scripts/index.js
+++ scripts/index.js
@@ -27,7 +27,24 @@ var closeModal = () => {
   container.style.display = "none"
 }
 
-var openModal = () => {
-  var container = document.getElementById("modal-pop-up")
+var openModal = (e) => {
+  console.log(e.classList)
+  var classes = e.classList
+  var h2 = document.getElementById("modal-header")
+  var pop_up_message_container = document.getElementById("pop_up_message")
+  pop_up_message_container.innerHTML = ''
+  pop_up_message_container.innerHTML += "<br/>Dear Customer, <br/><br/>"
+
+  if (classes.contains("incident")) {
+      h2.innerText = "Incident Notification"
+      
+  } else {
+      h2.innerText = "Maintenance Notification"
+      pop_up_message_container.innerHTML += "Kindly be informed, we will be carrying out an emergency maintenance on our servers.<br/> <br/>"
+  }
+
+  var container = document.getElementById("modal_pop_up")
+  
+  pop_up_message_container.innerHTML += e.children[3].innerHTML
   container.style.display = "block"
 }
